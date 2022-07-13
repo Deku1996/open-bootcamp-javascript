cf50339cd6b6a2f3da074bb4805ba59d3dba542f
Deku1996 <cristiansantacruz72017@gmail.com>
2022-07-12 21:40:44 -0300

    doc: exc-1 javascript OpenBootcamp

---
+++ Tareas/exc-1.js
@@ -0,0 +1,11 @@
+var miNombre = "Cristian";
+var miEdad = 26;
+var desarrollador = false;
+var miFechaNacimiento =  new Date (22, 03, 1996);
+const favoriteBook = {
+autor: "Edgar Alan Poe",
+nombre: "El baul de los malos recuerdos",
+fecha: new Date (),
+link: "https//cualquierlink.com"
+
+} ;
