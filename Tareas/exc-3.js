0c327f375ef1daa11941a222464ed1fdac58594d
Deku1996 <cristiansantacruz72017@gmail.com>
2022-07-15 01:07:18 -0300

    doc:exc-3 strings

---
+++ Tareas/exc-3.js
@@ -0,0 +1,26 @@
+let miNombre = "Hola, mi nombre es Cristian"
+let miApellido = "Hola mi apellido es Santa Cruz"
+
+let estudiante = `${miNombre}. ${miApellido}  `
+console.log(estudiante)
+
+let estudianteMayus = estudiante.toUpperCase();
+console.log(estudianteMayus)
+
+let estudianteMinus = estudiante.toLowerCase();
+console.log(estudianteMinus)
+
+let espacios = estudiante.length;
+console.log(espacios)
+
+let primeraLetra = miNombre.charAt();
+console.log(primeraLetra)
+
+let ultimaLetra = miApellido.lastIndexOf()
+console.log(ultimaLetra)
+
+let eliminarEspacios = estudiante.trim.length;
+console.log(eliminarEspacios)
+
+let boolean = estudiante.includes("Cristian");
+console.log(boolean)
