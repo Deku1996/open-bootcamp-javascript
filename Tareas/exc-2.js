ec26f1e6dc9747a6b60ea30fff6b0cbad4a0f197
Deku1996 <cristiansantacruz72017@gmail.com>
2022-07-14 21:22:04 -0300

    doc: exc-2

---
+++ Tareas/exc-2.js
@@ -0,0 +1,26 @@
+let notas = [2, 4, 5, 6, 8, 11, 12];
+
+for (let i = 0; i < notas.length; i++ ){
+    console.log([i])
+}
+
+
+let maximo = 54;
+let minimo = 8;
+
+while (minimo < maximo) {
+    console.log(minimo)
+    minimo++
+}
+
+let listaUno = 9;
+let listaDos = 0;
+
+while  (listaDos < listaUno){
+    console.log(listaDos)
+    listaDos++
+    if (listaUno === 5){
+        break
+        }
+        console.log(listaUno)
+    }
