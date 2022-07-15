aafee2e39354ca69f3da165cb93f0351967e43c6
Deku1996 <cristiansantacruz72017@gmail.com>
2022-07-14 21:00:48 -0300

    doc: switch

---
+++ part-two/Sentencias-switch.js
@@ -0,0 +1,18 @@
+//Sentencias Switch
+
+let nota = 7;
+
+switch (nota){
+    case 5:
+        console.log( "Haz aprobado por los pelos");
+        break;
+    case 4:
+        console.log ( "Esfuerzate mas");
+        break;
+    case 7:
+        console.log ("Bien hecho");
+        break;
+    default:
+        break;
+
+    }
