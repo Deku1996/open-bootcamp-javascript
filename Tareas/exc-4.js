7084c6f14a94158f7bc4cd3631635fdb293ee02f
Deku1996 <cristiansantacruz72017@gmail.com>
2022-07-16 00:49:20 -0300

    doc: exc 4

---
+++ Tareas/exc-4.js
@@ -0,0 +1,7 @@
+let altura = 196;
+let alturaMetros = 1.96;
+let peso = 110;
+let redondeo = Math.ceil(alturaMetros);
+let redondeoDos = Math.floor(alturaMetros);
+let maximo = Number.MAX_VALUE + 1;
+
