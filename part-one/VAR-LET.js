72f9b4e41d8287827e4e11bd5083cf701fa6f930
Deku1996 <cristiansantacruz72017@gmail.com>
2022-07-16 16:50:54 -0300

    doc: other

---
+++ part-one/VAR-LET.js
@@ -0,0 +1,26 @@
+var variable;
+//let variableLet;
+
+const constante = "soy una constante"; //no se puede reasignar
+
+let b = 5;
+console.log(b);
+
+
+
+var variable = "hola soy la primera variable";
+
+for (var i = 0; i < 3; i++){
+    variable = "soy la segunda variable"
+}
+console.log(variable);
+
+// DIFERENCIA ENTRE VAR Y LET
+
+let variableLet = "Hola soy la primera variable LeT"
+for (var i = 0; i < 3; i++){
+   let variableLet = "Hola soy la segunda variable LET"
+}
+console.log(variableLet);
+
+
