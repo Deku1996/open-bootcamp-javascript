4df71eb3d886bb994438b5668f60ab704cf744bb
Deku1996 <cristiansantacruz72017@gmail.com>
2022-07-18 11:15:30 -0300

    doc: exc-6

---
+++ Tareas/exc-6.js
@@ -0,0 +1,6 @@
+const arrayFamily = [ "Graciela",  "Francisco", "Sherilin", "Cristian", "Marisa" ];
+const newSet = new Set(arrayFamily)
+newSet.add("Cristian")
+console.log(newSet)
+newSet.add("Javascript");
+console.log(newSet)
